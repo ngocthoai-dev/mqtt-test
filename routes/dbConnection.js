813263c1210102d1c8fa5379003a15bf340c1ac4
TachibanaKanade99 <sad361975@gmail.com>
2020-06-09 23:03:12 +0700

    Change uri to url - Test commit

--- routes/dbConnection.js
+++ routes/dbConnection.js
@@ -3,14 +3,14 @@ var _db;
 // connect db, if success listen to port
 let initDbConection = function(callback){
   const MongoClient = require('mongodb').MongoClient;
-  const uri = "mongodb+srv://dbMqttTest:PNThoai9x@cluster0-u7lvh.mongodb.net/test?retryWrites=true&w=majority";
+  const url = "mongodb+srv://dbMqttTest:PNThoai9x@cluster0-u7lvh.mongodb.net/test?retryWrites=true&w=majority";
   const mongoOptions = {
     poolSize: 1000,
     promiseLibrary: global.Promise,
     useUnifiedTopology: true
   };
 
-  MongoClient.connect(uri, mongoOptions, function(err, client) {
+  MongoClient.connect(url, mongoOptions, function(err, client) {
     if(err) throw err;
     _db = client.db('dbMqttTest');
     callback();
