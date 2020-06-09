7c9f524fe66cfaa2a34b0ea36d688065b1d72726
TachibanaKanade99 <sad361975@gmail.com>
2020-06-09 23:05:06 +0700

    Modify OAuth2 - add transporter.set for OAuth2

--- routes/msgAlert.js
+++ routes/msgAlert.js
@@ -1,22 +1,36 @@
 const db = require('../routes/dbConnection').db;
 
 const nodemailer = require('nodemailer');
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: 'guest121019@gmail.com',
-    clientId: '490570341278-6lobi6mjkok82li9gd4fran7f31m9rjd.apps.googleusercontent.com',
-    clientSecret: 't1sxhDBkE_SF9ztil7A7Tuq5',
-    refreshToken: '1//046tYAusmP_amCgYIARAAGAQSNwF-L9Ir8WS80p1ITDKISHYFlo9m_Y_gFNxwUaOSYOKmYu-KKuWG3NTwt1cW0_B-fJjhkWFdVTE',
-    accessToken: 'ya29.a0AfH6SMBVD5Ls4O0nHgHF4TNiaQrn_PxUHUKZN8MV2SpMDkfInvYU_Mw1blTbtz5zSUl0b8HO7FE-9tnIMDKjZR4EYTEWiWhgUl38mYYn1mMozUxp1HTcOBXwTTM2xqIsrp2trnSNB7MzQ0KVqq-x9KONDZaXi00HCxs',
-  }
+let transporter = nodemailer.createTransport({
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true,
+	service: 'gmail',
+  	auth: {
+		type: 'OAuth2',
+		user: 'guest121019@gmail.com',
+		clientId: '490570341278-6lobi6mjkok82li9gd4fran7f31m9rjd.apps.googleusercontent.com',
+		clientSecret: 't1sxhDBkE_SF9ztil7A7Tuq5',
+		refreshToken: '1//046tYAusmP_amCgYIARAAGAQSNwF-L9Ir8WS80p1ITDKISHYFlo9m_Y_gFNxwUaOSYOKmYu-KKuWG3NTwt1cW0_B-fJjhkWFdVTE',
+		accessToken: 'ya29.a0AfH6SMBVD5Ls4O0nHgHF4TNiaQrn_PxUHUKZN8MV2SpMDkfInvYU_Mw1blTbtz5zSUl0b8HO7FE-9tnIMDKjZR4EYTEWiWhgUl38mYYn1mMozUxp1HTcOBXwTTM2xqIsrp2trnSNB7MzQ0KVqq-x9KONDZaXi00HCxs',
+		expires: 1484314697598
+  	}
 });
 
-function sendMsg(from, to, callback, title='welcome!', content='hello'){
+transporter.set('oauth2_provision_cb', (user, renew, callback) => {
+	let accessToken = userTokens[user];
+	if (!accessToken){
+		return callback(new Error('Unknown user'));
+	}
+	else{
+		return callback(null, accessToken);
+	}
+});
+
+function sendMsg(to, callback, title='welcome!', content='hello'){
   let mailOptions = {
-    from: from,
-    to: to,
+	from: 'guest121019@gmail.com',
+	to: to,
     subject: title,
     text: content,
   };
@@ -25,7 +39,7 @@ function sendMsg(from, to, callback, title='welcome!', content='hello'){
     if (error) {
       callback(error);
     } else {
-      callback('Email sent: ' + info);
+      callback('Email sent: ' + info.response);
     }
   });
 }
@@ -47,13 +61,12 @@ let checkInDangerTree = setInterval(()=>{
               if(err) throw err;
 
               if(user.gmail){
-                sensMsg('guest121019@gmail.com', user.gmail, function(msg){
-                  console.log(msg);
-                }, 'ALERT: ' + tree.name + ' Need Water!',
-                ('Dear ' + user.username + ',<br/><br/>' +
+                var title = 'ALERT: ' + tree.name + ' Need Water!'; 
+                var msg = 'Dear ' + user.username + ',<br/><br/>' +
                 'the ' + tree.name + ' tree with the temperature: <b>' + temp + '</b>;' + 'the moisture: <b>' + mois + '</b>;' + 'the humidity: <b>' + humi + '</b>;' +
                 'need watering!<br/><br/>' +
-                'Thanks for using our app,<br/>Sincerely,<br/>mqtt-test.'));
+                'Thanks for using our app,<br/>Sincerely,<br/>mqtt-test.';
+                sendMsg(user.gmail, callback, title, msg);
               }
             });
           });
